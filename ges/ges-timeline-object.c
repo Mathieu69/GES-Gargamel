cf3b4e627479b064d80b38669584c11f506e990c
Mathieu Duponchelle <seeed@laposte.net>
2011-08-28 02:05:41 +0200

    Make the track_object's timeline object be set in timeline_object_add_track_object

--- ges/ges-timeline-object.c
+++ ges/ges-timeline-object.c
@@ -437,6 +437,8 @@ ges_timeline_object_add_track_object (GESTimelineObject * object, GESTrackObject
   if (!trobj)
     return FALSE;
 
+  ges_track_object_set_timeline_object (trobj, object);
+
   g_object_ref (trobj);
 
   mapping = g_slice_new0 (ObjectMapping);
@@ -514,10 +516,10 @@ ges_timeline_object_add_track_object (GESTimelineObject * object, GESTrackObject
       + mapping->priority_offset);
 
   GST_DEBUG ("Returning trobj:%p", trobj);
-
-  g_signal_emit (object, ges_timeline_object_signals[TRACK_OBJECT_ADDED], 0,
-      GES_TRACK_OBJECT (trobj));
-
+  if (!GES_IS_TRACK_PARSE_LAUNCH_EFFECT (trobj)) {
+    g_signal_emit (object, ges_timeline_object_signals[TRACK_OBJECT_ADDED], 0,
+        GES_TRACK_OBJECT (trobj));
+  }
   return TRUE;
 }
 
